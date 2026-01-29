813864bea1c5aa8d3396defbfb9d63b0d15557d3
Aditya Singh <adityasinghboss1234@gmail.com>
2026-01-29 13:03:23 +0530

    Implement HTTP chunked transfer encoding for streaming responses
    Add support for Transfer-Encoding: chunked to enable streaming of responses
    where content length is unknown upfront. This allows the server to proxy
    streaming APIs (like httpbin.org/stream) without buffering the entire response.
    
    - Add /httpbin/stream/* route that proxies requests to httpbin.org
    - Remove Content-Length header and set Transfer-Encoding: chunked
    - Stream response in chunks with hex size prefix format
    - Send terminating chunk to signal end of stream
    - Read upstream response in 32-byte chunks to demonstrate streaming
    - Handle errors by falling back to 500 response
    
    Chunked encoding sends data as: size-in-hex CRLF data CRLF, ending with
    zero-length chunk (0 CRLF CRLF). This enables real-time streaming without
    knowing the total response size, essential for long-lived connections,
    SSE, and proxying streaming APIs.

--- cmd/httpServer/main.go
+++ cmd/httpServer/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/Adi-ty/http-from-scratch/internal/request"
@@ -62,6 +64,34 @@ func main() {
 		} else if req.RequestLine.RequestTarget == "/woopsie-daisy" {
 			body = responsd500()
 			status = response.StatusInternalServerError
+		} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/stream") {
+			target := req.RequestLine.RequestTarget
+
+			res, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
+			if err != nil {
+				body = responsd500()
+				status = response.StatusInternalServerError
+			} else {
+				w.WriteStatusLine(response.StatusOK)
+				h.Delete("Content-Length")
+				h.Set("transfer-encoding", "chunked")
+				h.Replace("content-type", "text/plain")
+				w.WriteHeaders(*h)
+				
+				for {
+					data := make([]byte, 32)
+					n, err := res.Body.Read(data)
+					if err != nil {
+						break
+					}
+
+					w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
+					w.WriteBody(data[:n])
+					w.WriteBody([]byte("\r\n"))
+				}
+				w.WriteBody([]byte("0\r\n\r\n"))
+				return
+			}
 		}
 
 		h.Replace("Content-Length", fmt.Sprintf("%d", len(body)))
