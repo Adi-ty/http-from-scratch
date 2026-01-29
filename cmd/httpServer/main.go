0d6e802fb4afb0388848c430539d5345fb4fe278
Aditya Singh <adityasinghboss1234@gmail.com>
2026-01-29 13:29:30 +0530

    feat: implement HTTP streaming with chunked transfer encoding and trailers for SHA256 and content length

--- cmd/httpServer/main.go
+++ cmd/httpServer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/Adi-ty/http-from-scratch/internal/headers"
 	"github.com/Adi-ty/http-from-scratch/internal/request"
 	"github.com/Adi-ty/http-from-scratch/internal/response"
 	"github.com/Adi-ty/http-from-scratch/internal/server"
@@ -64,7 +66,7 @@ func main() {
 		} else if req.RequestLine.RequestTarget == "/woopsie-daisy" {
 			body = responsd500()
 			status = response.StatusInternalServerError
-		} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/stream") {
+		} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/") {
 			target := req.RequestLine.RequestTarget
 
 			res, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
@@ -76,8 +78,11 @@ func main() {
 				h.Delete("Content-Length")
 				h.Set("transfer-encoding", "chunked")
 				h.Replace("content-type", "text/plain")
+				h.Set("Trailer", "X-Content-SHA256")
+				h.Set("Trailer", "X-Content-Length")
 				w.WriteHeaders(*h)
 				
+				fullBody := []byte{}
 				for {
 					data := make([]byte, 32)
 					n, err := res.Body.Read(data)
@@ -85,11 +90,17 @@ func main() {
 						break
 					}
 
+					fullBody = append(fullBody, data[:n]...)
 					w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
 					w.WriteBody(data[:n])
 					w.WriteBody([]byte("\r\n"))
 				}
-				w.WriteBody([]byte("0\r\n\r\n"))
+				w.WriteBody([]byte("0\r\n"))
+				trailers := headers.NewHeaders()
+				trailers.Set("X-Content-SHA256", fmt.Sprintf("%x", sha256.Sum256(fullBody)))
+				trailers.Set("X-Content-Length", fmt.Sprintf("%d", len(fullBody)))
+				w.WriteHeaders(*trailers)
+
 				return
 			}
 		}
